fda0e73d9a265abdcada2329796dd5dc2e940d8a
Uziel Hernandez <uzielmelitonh@gmail.com>
2022-11-22 11:58:01 -0600

    feat: input date picker desktop

---
+++ components/common/input/InputDatePickerDesktop.jsx
@@ -0,0 +1,40 @@
+import { DesktopDatePicker } from '@mui/x-date-pickers'
+import IconDate from 'components/icons/IconDate'
+import { BpTextField } from 'components/shared'
+import { useState } from 'react'
+
+const InputDatePickerDesktop = ({
+  value,
+  helperText,
+  id,
+  name,
+  toolbarTitle = 'Selecciona fecha',
+  views = ['year', 'day', 'month'],
+  placeholder,
+  error,
+  onChange,
+  ...props
+}) => (
+  <DesktopDatePicker
+    value={value}
+    onChange={onChange}
+    views={views}
+    toolbarTitle={toolbarTitle}
+    components={{
+      OpenPickerIcon: IconDate
+    }}
+    {...props}
+    renderInput={params => (
+      <BpTextField
+        {...params}
+        id={id}
+        name={name}
+        helperText={helperText}
+        placeholder={placeholder}
+        error={error}
+        color="primary"
+      />
+    )}
+  />
+)
+export default InputDatePickerDesktop
--- components/common/input/index.js
+++ components/common/input/index.js
@@ -1,4 +1,6 @@
 export { default as InputDatePicker } from './InputDatePicker'
+export { default as InputDatePickerDesktop } from './InputDatePickerDesktop'
 export { default as InputNumber } from './InputNumber'
 export { default as InputPassword } from './InputPassword'
+export { default as InputRadio } from './InputRadio'
 export { default as InputRating } from './InputRating'
